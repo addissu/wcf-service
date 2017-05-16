652a20a871628f81076d333a91adb4ec3e5d6558
addissu <atamru05@gmail.com>
2017-05-16 12:24:59 -0700

    Add files via upload

---
+++ App_Code/CommunityService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CommunityService" in code, svc and config file together.
+public class CommunityService : ICommunityService
+{
+    Community_AssistEntities db = new Community_AssistEntities();
+
+    
+
+
+
+    public int Login(string email, string password)
+    {
+        {
+            int key = 0;
+            int result = db.usp_Login(email, password);
+
+            //Community_AssistEntities db = new Community_AssistEntities();
+            //int success = db.usp_Login(email, password);
+
+            if (result != -1)
+            {
+                var uKey = (from k in db.People
+                            where k.PersonEmail.Equals(email)
+                            select k.PersonKey).FirstOrDefault();
+
+                key = (int)uKey;
+                
+
+            }
+
+            return key;
+
+        }
+    }
+
+    public bool newGrant(GrantRequest g)
+    {
+        db.GrantRequests.Add(g);
+        db.SaveChanges();
+        return true;
+    }
+
+    public int RegisterUser(personInfo p)
+    {
+
+        var perinfo = from b in db.People
+
+                      select b;
+        List<Person> person = new List <Person>();
+        // List<personInfo> per = new List<personInfo>();
+        //bool result = true;
+        int result = db.usp_Register(p.lastName, p.fisrtName, p.email, p.password, p.apartmentNumber, p.street, p.city, p.state, p.zipCode, p.homePhone, p.workPhone);
+
+
+        return result;
+
+    }
+
+    public List<GrantRequest> ReviewGrant (int personKey)
+    {
+        var rev = from b in db.GrantRequests  where b.PersonKey==personKey select b;
+        List<GrantRequest> grat = new List<GrantRequest>();
+
+        foreach(var r in rev)
+        {
+            GrantRequest gr = new GrantRequest();
+            gr.GrantRequestAmount = r.GrantRequestAmount;
+            gr.GrantRequestDate = r.GrantRequestDate;
+            gr.GrantRequestExplanation = r.GrantRequestExplanation;
+            gr.PersonKey = r.PersonKey;
+
+            grat.Add(gr); 
+
+        }
+        return grat;
+    }
+
+
+    public List<GrantReviewComment> viewGrants(string listofgrants)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ App_Code/ICommunityService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICommunityService" in both code and config file together.
+[ServiceContract]
+public interface ICommunityService
+{
+    [OperationContract]
+    int Login(string email, string password);
+
+    [OperationContract]
+    int RegisterUser(personInfo p);
+
+    [OperationContract]
+
+    List<GrantRequest> ReviewGrant(int pKey);
+
+    [OperationContract]
+
+    List<GrantReviewComment> viewGrants (string listofgrants);
+
+    [OperationContract]
+
+    bool newGrant(GrantRequest g);
+}
+
+[DataContract]
+
+public class ApplyGrant
+{
+    [DataMember]
+    public List<GrantRequest> apply {get; set;}
+}
+
+public class viewGrant
+{
+    [DataMember]
+    public List<GrantReviewComment> type { get; set; }
+}
+
+public class personInfo
+{
+    [DataMember]
+    public string lastName { get; set; }
+
+    [DataMember]
+    public string fisrtName { get; set; }
+
+    [DataMember]
+    public string email { get; set; }
+
+    [DataMember]
+    public string password { get; set; }
+
+    [DataMember]
+    public string apartmentNumber { get; set; }
+
+    [DataMember]
+    public string street { get; set; }
+
+    [DataMember]
+    public string city { get; set; }
+
+    [DataMember]
+    public string state { get; set; }
+
+    [DataMember]
+    public string zipCode { get; set; }
+
+    [DataMember]
+    public string homePhone { get; set; }
+
+    [DataMember]
+    public string workPhone { get; set; }
+}
